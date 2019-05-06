d05c3c7d31524aaa441f044211e9836d1038521a
EIN0043 <EIN0043@ESL008.jstlglobal.com>
2019-05-06 11:33:40 +0530

    jdbc connection

---
+++ src/main/java/Common/JDBC_Connection.java
@@ -0,0 +1,27 @@
+package Common;
+
+public class JDBC_Connection {
+	/*Connection conn = null;
+	Statement stmt = null;
+	ResultSet resultSet = null;
+	String query = String.format("select PAN from users where phone_home=\"%s\"",phone_num);
+	System.out.println(query);
+	String Pan_no="";
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+	    e.printStackTrace();
+	}
+
+	conn = DriverManager.getConnection("jdbc:mysql://52.201.100.74:3306/cmol_uat", "developer", "D3V3L0P3R@321go");
+	stmt = conn.createStatement();
+	resultSet = stmt.executeQuery(query);
+	while (resultSet.next()) {
+	    Pan_no =resultSet.getString("PAN");
+	}
+	resultSet.close();
+	stmt.close();
+	conn.close();
+*/
+
+}
